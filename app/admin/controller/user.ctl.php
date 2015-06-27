6d941be828c37c6d70e3e8126ec2990e632ee3d8
unknown <001424@zy001424-1.zjzxlh.cn>
2015-06-27 14:12:36 +0800

    '人员管理'

---
+++ app/admin/controller/user.ctl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 001424
+ * Date: 2015/6/27
+ * Time: 12:24
+ */
+
+class UserController extends BaseController{
+
+    public function listAction(){
+        $this->show('user_list');
+    }
+}
---
+++ app/admin/view/user_list.view.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hive - 人员管理</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.4 -->
+    <link href="static/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- FontAwesome 4.3.0 -->
+    <link href="static/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons 2.0.0 -->
+    <link href="static/plugins/ionicons/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="static/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link href="static/dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+    <!-- iCheck -->
+    <link href="static/plugins/iCheck/flat/blue.css" rel="stylesheet" type="text/css" />
+    <!-- jvectormap -->
+    <link href="static/plugins/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+    <!-- Date Picker -->
+    <link href="static/plugins/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
+    <!-- Daterange picker -->
+    <link href="static/plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link href="static/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="static/plugins/html5shiv/html5shiv.min.js"></script>
+    <script src="static/plugins/respond/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="skin-blue sidebar-mini">
+<div class="wrapper">
+
+    <?php include("header.view.php"); ?>
+    <?php include("menu.view.php"); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                人员列表
+                <small>系统管理</small>
+            </h1>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="box box-primary">
+                        <div class="box-header with-border">
+                            <h3 class="box-title"></h3>
+                            <div class="box-tools">
+                                <div class="input-group">
+                                    <input type="text" placeholder="Search" style="width: 150px;" class="form-control input-sm pull-right" name="table_search">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
+                                        <button class="btn btn-sm btn-default">新成员</button>
+                                        <button class="btn btn-sm btn-default">删除</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- /.box-header -->
+                        <div class="box-body table-responsive no-padding">
+                            <table class="table table-hover">
+                                <tbody><tr>
+                                    <th>ID</th>
+                                    <th>登录名</th>
+                                    <th>姓名</th>
+                                    <th>状态</th>
+                                    <th>参与项目</th>
+                                    <th>参与单子数</th>
+                                    <th>打回数</th>
+                                    <th>回滚数</th>
+                                    <th>注册时间</th>
+                                </tr>
+                                <tr>
+                                    <td>175</td>
+                                    <td>zhangyuan@163.com</td>
+                                    <td>张园</td>
+                                    <td><span class="label label-danger">中止</span></td>
+                                    <td>项目A、项目B</td>
+                                    <td>1200</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>2014/05/07</td>
+                                </tr>
+                                </tbody></table>
+                        </div>
+                        <div class="box-footer clearfix">
+                            <ul class="pagination pagination-sm no-margin pull-right">
+                                <li><a href="#">«</a></li>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">»</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button aria-label="Close" data-dismiss="modal" class="close" type="button"><span aria-hidden="true">×</span></button>
+                            <h4 class="modal-title">Modal Default</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>One fine body…</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button data-dismiss="modal" class="btn btn-default pull-left" type="button">Close</button>
+                            <button class="btn btn-primary" type="button">Save changes</button>
+                        </div>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div>
+        </section><!-- /.content -->
+    </div><!-- /.content-wrapper -->
+
+    <?php include('footer.view.php');?>
+    <?php include("sidebar.view.php"); ?>
+</div><!-- ./wrapper -->
+
+<!-- jQuery 2.1.4 -->
+<script src="static/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<!-- jQuery UI 1.11.2 -->
+<script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="static/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- Sparkline -->
+<script src="static/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+<!-- jvectormap -->
+<script src="static/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
+<script src="static/plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+<!-- jQuery Knob Chart -->
+<script src="static/plugins/knob/jquery.knob.js" type="text/javascript"></script>
+<!-- daterangepicker -->
+<script src="static/plugins/moment/moment.min.js" type="text/javascript"></script>
+<script src="static/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+<!-- datepicker -->
+<script src="static/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="static/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
+<!-- Slimscroll -->
+<script src="static/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<!-- FastClick -->
+<script src='static/plugins/fastclick/fastclick.min.js'></script>
+<!-- AdminLTE App -->
+<script src="static/dist/js/app.min.js" type="text/javascript"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="static/dist/js/demo.js" type="text/javascript"></script>
+</body>
+</html>
